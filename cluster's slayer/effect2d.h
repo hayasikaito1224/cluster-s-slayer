559696c29701582fd5234d256b4ef9f72bf6fd5a
Nsparrow000 <201052@st.yoshida-g.ac.jp>
2022-05-23 15:53:19 +0900

    2d

---
+++ cluster's slayer/effect2d.h
@@ -0,0 +1,41 @@
+//*****************************************************************************
+// エフェクト処理 [Effect.h]
+// Author : 佐藤秀亮
+//*****************************************************************************
+
+#ifndef _EFFECT_H_
+#define _EFFECT_H_
+
+#include "Scene2D.h"
+
+class CEffect2d : public CScene2D
+{
+public:
+	CEffect2d(OBJTYPE nPlyorty = CScene::OBJTYPE_EFFECT);
+	virtual ~CEffect2d();
+
+	virtual HRESULT Init(D3DXVECTOR3 pos, D3DCOLORVALUE color, D3DCOLORVALUE Mincolor, D3DXVECTOR2 Size, D3DXVECTOR2 MinSize, int nLife, int nType, int Synthetic);
+	virtual void Uninit();
+	virtual void Update();
+	virtual void Draw();
+
+
+protected:
+	bool m_bUninit;
+	int m_nLife;
+	D3DXVECTOR2 m_Size;			//大きさ
+	D3DXVECTOR2 m_MinSize;		//大きさ変動
+
+private:
+
+	//カラー値
+	D3DCOLORVALUE m_Color;
+
+	//減らすカラー値
+	D3DCOLORVALUE m_MinColor;
+
+	D3DXVECTOR2 m_move;			//移動
+	int m_nSynthenic;	//合成
+
+};
+#endif
